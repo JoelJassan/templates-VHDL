0f58b94bf2a6f78995b34c00a9e9dcf4f05c3722
Joel Jassan <joeljassan@hotmail.com>
2023-05-26 19:56:06 -0300

    Se cargan los template

---
+++ tb_template.vhd
@@ -0,0 +1,90 @@
+-- VHDL file
+--
+-- Autor: Jassan, Joel
+-- Date: (mmm/YYYY)
+-- 
+-- Proyect Explanation:
+--
+--
+-- Copyright 2023, Joel Jassan <joeljassan@hotmail.com>
+-- All rights reserved.
+---------------------------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity tb_example is
+end entity;
+
+architecture a_tb_example of tb_example is
+
+    ----- Typedefs --------------------------------------------------------------------------------
+
+    ----- Constants -------------------------------------------------------------------------------
+    constant simulation_time : integer := 5000; --esto no funciona
+
+    ----- Simulation ------------------------------------------------------------------------------
+
+    ----- Signals (i: entrada, o:salida, s:señal intermedia) --------------------------------------
+    signal clk_i, rst_i, enable_i : std_logic;
+
+    --component inputs
+
+    --component outputs
+    
+begin
+    ----- Component to validate -------------------------------------------------------------------
+    ex_component : entity work.componente_ejemplo
+        generic map()
+        port map();
+    ----- Code ------------------------------------------------------------------------------------
+
+    -- clock stimulus
+    reloj : process
+    begin
+        clk_i <= '0';
+        wait for 10 ns;
+        clk_i <= '1';
+        wait for 10 ns;
+    end process;
+
+    -- reset stimulus
+    reset : process
+    begin
+        rst_i <= '0';
+        wait for 80 ns;
+        rst_i <= '1';
+        wait;
+    end process;
+
+    -- enable stimulus
+    enable : process
+    begin
+        enable_i <= '0';
+        wait for 100 ns; --espera habilitacion del reset
+        enable_i <= '1';
+        wait;
+    end process;
+
+    -- component to validate stimulus
+    --
+    --
+    ejecucion : process
+    begin
+        wait;
+    end process;
+    --
+    --
+    --
+	
+    -- End of test
+    stop : process
+    begin
+        wait for 5000 ns; --tiempo total de
+        std.env.stop;
+    end process;
+
+    -- Data Verify
+    -- aqui irian los note, warning, etc.
+
+end architecture;
---
+++ template.vhd
@@ -0,0 +1,53 @@
+-- VHDL file
+--
+-- Autor: Jassan, Joel
+-- Date: (mmm/YYYY)
+-- 
+-- Proyect Explanation:
+--
+--
+-- Copyright 2023, Joel Jassan <joeljassan@hotmail.com>
+-- All rights reserved.
+---------------------------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity ent is
+	
+	generic (
+
+    );
+
+	port (
+        --input ports
+		clk		  	: in std_logic;
+		reset	  	: in std_logic;
+		enable		: in std_logic;
+
+        --output ports
+    
+    );
+
+end entity;
+
+architecture a_ent of ent is
+
+    ----- Typedefs --------------------------------------------------------------------------------
+    
+    ----- Constants -------------------------------------------------------------------------------
+
+    ----- Signals (i: entrada, o:salida, s:señal intermedia)---------------------------------------
+
+
+begin
+    ----- Components ------------------------------------------------------------------------------
+
+    ----- Codigo ----------------------------------------------------------------------------------
+    
+    -- Logica Estado Siguiente
+
+    -- Logica Salida
+
+    
+end architecture ; 
